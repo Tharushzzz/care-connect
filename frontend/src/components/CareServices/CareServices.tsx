6dfd7558d80bccb63600d992bb8176ac19157125
THARUSHA <tharushadilmin123@gmail.com>
2026-03-25 19:19:35 +0530

    feat: Implement CareServices component with ServicesItem integration

--- frontend/src/components/CareServices/CareServices.tsx
+++ frontend/src/components/CareServices/CareServices.tsx
@@ -1,14 +1,21 @@
-
+import ServicesItem from "./ServicesItem"
 
 const CareServices = () => {
   return (
-    <section className='flex h-screen bg-linear-to-b from-[#0284C426] to-[#A5CACA]'>
-        <div className="flex flex-col p-15 gap-5">
-            <h1 className="font-bold text-3xl">Comprehensive Care Services</h1>
+    <section className='flex flex-col py-10 bg-linear-to-b from-[#0284C426] to-[#A5CACA]'>
+        <div className="flex flex-col justify-center pl-20 py-10 gap-5">
+            <h1 className="font-bold text-4xl">Comprehensive Care Services</h1>
             <p className="text-lg text-[#0D182B99] font-medium w-2/3">Our network of professionals covers a wide range of needs, ensuring your family member receives specialized attention tailored to their condition.</p>
         </div>
 
-        <div></div>
+        <div className="flex justify-center items-center gap-10 flex-wrap">
+            <ServicesItem />
+            <ServicesItem />
+            <ServicesItem />
+            <ServicesItem />
+            <ServicesItem />
+            <ServicesItem />
+        </div>
     </section>
   )
 }
---
+++ frontend/src/components/CareServices/ServicesItem.tsx
@@ -0,0 +1,13 @@
+
+
+const ServicesItem = () => {
+  return (
+    <div className="flex flex-col gap-2.5 w-106 h-80.5 bg-white border rounded-2xl p-6">
+        <div></div>
+        <div></div>
+        <div></div>
+    </div>
+  )
+}
+
+export default ServicesItem
