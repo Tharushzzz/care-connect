ab1b63907918dc044b561bb9d76a9cd11d3d9f28
THARUSHA <tharushadilmin123@gmail.com>
2026-03-25 20:05:31 +0530

    Fix: Add shadow and hover effects to ServicesItem component for improved interactivity

--- frontend/src/components/CareServices/ServicesItem.tsx
+++ frontend/src/components/CareServices/ServicesItem.tsx
@@ -2,7 +2,7 @@
 
 const ServicesItem = () => {
   return (
-    <div className="flex flex-col gap-2.5 w-106 h-80.5 bg-white border rounded-2xl p-6">
+    <div className="flex flex-col gap-2.5 w-106 h-80.5 bg-white rounded-2xl p-6 shadow-sm hover:shadow-xl transition-shadow ease-in-out cursor-pointer">
         <div></div>
         <div></div>
         <div></div>
