61e9ffc6098eb1e662827ff39c130e9ac588d833
THARUSHA <tharushadilmin123@gmail.com>
2026-03-25 20:05:43 +0530

    feat: Enhance CareServices and ServicesItem components with dynamic data rendering

--- frontend/src/components/CareServices/CareServices.tsx
+++ frontend/src/components/CareServices/CareServices.tsx
@@ -1,23 +1,32 @@
 import ServicesItem from "./ServicesItem"
+import { Bed } from 'lucide-react';
 
 const CareServices = () => {
+
+  const servicesData = [
+    {
+      id: 1,
+      icon: Bed,
+      header: "Hospital Bedside Care",
+      description: "Dedicated caregivers to stay by the bedside, monitor vitals, advocate for the patient, and provide companionship during hospital stays."
+    }
+  ]
+
   return (
-    <section className='flex flex-col py-10 bg-linear-to-b from-[#0284C426] to-[#A5CACA]'>
+    <section className='flex flex-col py-10 bg-linear-to-b from-white to-[#A5CACA]'>
         <div className="flex flex-col justify-center pl-20 py-10 gap-5">
             <h1 className="font-bold text-4xl">Comprehensive Care Services</h1>
             <p className="text-lg text-[#0D182B99] font-medium w-2/3">Our network of professionals covers a wide range of needs, ensuring your family member receives specialized attention tailored to their condition.</p>
         </div>
 
         <div className="flex justify-center items-center gap-10 flex-wrap">
-            <ServicesItem />
-            <ServicesItem />
-            <ServicesItem />
-            <ServicesItem />
-            <ServicesItem />
-            <ServicesItem />
+            {servicesData.map(service => (
+              <ServicesItem icon={service.icon} header={service.header} description={service.description} key={service.id}></ServicesItem>
+            ))}
         </div>
     </section>
   )
 }
+
 
 export default CareServices
--- frontend/src/components/CareServices/ServicesItem.tsx
+++ frontend/src/components/CareServices/ServicesItem.tsx
@@ -1,11 +1,18 @@
 
+import type { LucideIcon } from 'lucide-react'
 
-const ServicesItem = () => {
+const ServicesItem = ({ icon: Icon, header, description } : { icon: LucideIcon; header: string; description: string }) => {
   return (
-    <div className="flex flex-col gap-2.5 w-106 h-80.5 bg-white rounded-2xl p-6 shadow-sm hover:shadow-xl transition-shadow ease-in-out cursor-pointer">
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className="flex flex-col gap-4 w-106 h-80.5 bg-white rounded-2xl p-6 shadow-sm hover:shadow-xl transition-shadow ease-in-out cursor-pointer">
+        <div className='pb-5'>
+            <Icon size={50} className="text-[#0686CD] bg-[#f0f7ff] p-2 rounded-xl" />
+        </div>
+        <div className='font-semibold text-2xl'>
+            {header}
+        </div>
+        <div className='text-xl font-medium text-[#0D182B80]'>
+            {description}
+        </div>
     </div>
   )
 }
