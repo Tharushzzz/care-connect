0569b0fcc405250ddac2930cefcfc5955a202a72
THARUSHA <tharushadilmin123@gmail.com>
2026-04-17 10:49:10 +0530

    add whyusimage

--- frontend/src/components/Why_Us/WhyUs.tsx
+++ frontend/src/components/Why_Us/WhyUs.tsx
@@ -1,10 +1,16 @@
-
+import whyus from "../../assets/whyus.png";
 
 const WhyUs = () => {
   return (
-    <section className="flex items-center justify-center bg-linear-to-b from-[F4FEFF] to-[CCECEC]">
-        <div></div>
-        <div></div>
+    <section className="flex gap-3 items-center justify-center bg-linear-to-b from-[#F4FEFF] to-[#d0eeee] h-[91vh]">
+        <div className="flex w-1/2 h-full items-center justify-end pr-10">
+            <img src={whyus} alt="Why Us" className="object-cover h-[90%]" >
+              
+            </img>
+        </div>
+
+
+        <div className="flex w-1/2 h-full border-2"></div>
     </section>
   )
 }
