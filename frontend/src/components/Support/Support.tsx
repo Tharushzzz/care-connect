e0afa1b2c0483e53995501df6b42d4edf38dce7d
THARUSHA <tharushadilmin123@gmail.com>
2026-04-17 20:17:37 +0530

    complete Support

--- frontend/src/components/Support/Support.tsx
+++ frontend/src/components/Support/Support.tsx
@@ -3,8 +3,19 @@
 const Support = () => {
   return (
     <div className="flex items-center justify-center pt-10 md:h-158">
-      <div className="w-250 bg-[#0384C6] h-110 rounded-[70px] shadow-4xl shadow-[rgba(130, 190, 243, 0.49)]">
+      <div className="flex flex-col relative gap-5 p-20 items-center justify-between w-250 h-100 rounded-[50px] shadow-[0_20px_80px_rgba(2,132,199,0.4)] bg-[radial-gradient(circle_at_center,#4fd1ff_0%,#0284c7_50%,#075985_100%)]">
 
+        <div className="text-[45px] text-white font-semibold">
+            Your loved one deserves the best care.
+        </div>
+        <div className="text-[20px] text-white text-center px-20 font-normal">
+            Join thousands of families who trust CareConnect for safe, reliable, and professional caregiving services.
+        </div>
+        <div className="flex justify-center items-center py-2 px-8 border-2 border-white rounded-3xl shadow-2xl">
+            <span className="text-xl text-white font-medium">Contact Support </span>
+        </div>
+        <div className="w-40 h-40 bg-white/10 rounded-full absolute -top-8 -left-8"></div>
+        <div className="w-40 h-40 bg-white/10 rounded-full absolute -bottom-8 -right-8"></div>
       </div>
     </div>
   )
