2d3452edfe1b87e51ec92bd222d173e1fd590e1f
THARUSHA <tharushadilmin123@gmail.com>
2026-07-18 17:46:36 +0530

    add reacr router

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,21 +1,17 @@
 import './App.css'
-import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
-import Home from './pages/Home.tsx'
 import Header from './components/Header/Header.tsx';
 import Footer from './components/Footer/Footer.tsx';
+import { Outlet } from 'react-router-dom';
 
 
 
 function App() {
   return (
-    <Router>
+    <>
       <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="*" element={<Navigate to="/" />} />
-      </Routes>
+      <Outlet></Outlet>
       <Footer />
-    </Router>
+    </>
   )
 }
 
