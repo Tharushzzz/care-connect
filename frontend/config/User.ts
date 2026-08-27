779bf7d615fcfa4f901e17f173277b79502350f9
THARUSHA <tharushadilmin123@gmail.com>
2026-08-27 16:25:10 +0530

    add some dump user data

---
+++ frontend/config/User.ts
@@ -0,0 +1,50 @@
+
+
+
+export type User= {
+    id:string,
+    name:string,
+    email:string,
+    phone:string,
+    avatar:string,
+    password:string
+}
+
+
+const UsersData: User[] =[
+    {
+        id:'usr_101',
+        name:"Elenor vence",
+        email:"elenor@gmail.com",
+        phone:"0712554567",
+        avatar:"",
+        password:"1234"
+    },
+    {
+        id:'usr_102',
+        name:"william",
+        email:"william@gmail.com",
+        phone:"0712554567",
+        avatar:"",
+        password:"1234"
+    },
+    {
+        id:'usr_103',
+        name:"william",
+        email:"william@gmail.com",
+        phone:"0712554567",
+        avatar:"",
+        password:"1234"
+    },
+    {
+        id:'usr_104',
+        name:"william",
+        email:"william@gmail.com",
+        phone:"0712554567",
+        avatar:"",
+        password:"1234"
+    }
+]
+
+
+export default UsersData
