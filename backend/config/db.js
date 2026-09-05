2be174edf67e3cfaaa724e05fd8a3c7ab6679ec5
THARUSHA <tharushadilmin123@gmail.com>
2026-09-05 21:09:44 +0530

    finish mongodb connect

---
+++ backend/config/db.js
@@ -0,0 +1,17 @@
+import mongoose from 'mongoose';
+import dns from 'dns';
+
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
+
+const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(process.env.MONGO_URI);
+    console.log(`MongoDB Connected: ${conn.connection.host}`);
+  } catch (error) {
+    console.error(`Error connecting to MongoDB: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+export default connectDB;
--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,25 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import connectDB from './config/db.js';
+import dns from "dns";
+
+dns.setServers(["1.1.1.1", "8.8.8.8"]);
 
 dotenv.config();
 
+// Connect to MongoDB
+connectDB();
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 
+// Body parser middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.send('CareConnect API is running...');
 });
 
 app.listen(port, () => {
