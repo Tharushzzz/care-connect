dcc552999caecc06d5d6844c5f470e45b3ac9160
reshaniraveena <reshaniraveena16@gmail.com>
2026-09-07 14:17:44 +0530

    Fix bug

--- frontend/src/pages/AdminDashboard.tsx
+++ frontend/src/pages/AdminDashboard.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { Link, useNavigate, useLocation } from 'react-router-dom';
 import logo from '../assets/logo/Logo.svg';
+// import lucide icons
 import {
   LayoutDashboard,
   Users,
